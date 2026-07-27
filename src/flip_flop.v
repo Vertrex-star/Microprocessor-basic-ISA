38f3abaa6d13690f27f340e0a9bb9bd90dc0ccb8
Paul Vincent Bezzina <109743463+Vertrex-star@users.noreply.github.com>
2026-07-27 22:38:47 +0300

    Update flip_flop.v

--- src/flip_flop.v
+++ src/flip_flop.v
@@ -1,10 +1,15 @@
-module flip_flop (input a, input clk, output b);
-reg out = 0;
-
-always @(posedge clk) begin 
+module flip_flop (
+    input      [7:0] a,
+    input            clk,
+    output     [7:0] b
+);
+ 
+reg [7:0] out = 8'd0;
+ 
+always @(posedge clk) begin
 	out <= a;
 end
-
-assign b = out; 
-
-endmodule 
+ 
+assign b = out;
+ 
+endmodule
